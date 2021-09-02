cb30b2e95de1c335ecdaf6c409374d7755554d71
Darío Marcos <37598162+ZenithGD@users.noreply.github.com>
2021-09-02 19:45:00 +0000

    Add premake script

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+out_dir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "ImGUI"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ( "bin/" .. out_dir .. "/%{prj.name}")
+    objdir ( "bin-int" .. out_dir .. "/%{prj.name}")
+
+    files {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_internal.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imsth_truetype.h",
+        "imgui_demo.cpp"        -- for quick testing purposes
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+
+    filter "system:linux"
+        systemversion "latest"
+        cppdialect "C++17"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
